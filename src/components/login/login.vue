3190463b06b95d4f83bfee6775380d16b2ae94a8
zhangruqi777 <690326277@qq.com>
2019-10-22 17:28:48 +0800

    新建登录分支路由

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,11 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div>登录hhhhhhh</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      path: '/login',
+      component: Login
     }
   ]
 })
