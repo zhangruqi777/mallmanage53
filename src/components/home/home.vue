0ab967cb63a72934cf6969f418366fabfc2f2748
zhangruqi777 <690326277@qq.com>
2019-10-25 15:58:54 +0800

    引入导航组件调整样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,54 +1,125 @@
 <template>
-    <el-container class="container">
-      <el-header class="header">
-          <el-row>
-            <el-col :span="4">
-                <div class="grid-content bg-purple">
-                    <img src="../../assets/logo.png" alt="无法显示图片">
-                </div>
-            </el-col>
-            <el-col :span="18" class="middle">
-                    <h2>后台管理系统</h2>
-            </el-col>
-            <el-col :span="28">
-                <div class="grid-content bg-purple">
-                    <a href="#" class="loginout">退出</a>
-                </div>
-            </el-col>
-          </el-row>
-      </el-header>
-      <el-container>
-        <el-aside class="aside" width="200px">Aside</el-aside>
-        <el-main class="main">Main</el-main>
-      </el-container>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <!-- <img src="../../assets/logo.png" alt="无法显示图片" /> -->
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h2>后台管理系统</h2>
+        </el-col>
+        <el-col :span="28">
+          <div class="grid-content bg-purple">
+            <a href="#" class="loginout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">
+        <!--侧面导航-->
+        <el-menu
+        :unique-opened=true>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-s-grid"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!--guide 2-->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-s-grid"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-location"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!--guide 3-->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-location"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-location"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-location"></i>
+              <span>商品分类</span>
+            </el-menu-item>            
+          </el-submenu>
+          <!--guide 4-->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-location"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!--guide 5-->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+              <i class="el-icon-location"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
 .container {
-    height: 100%;
-    background-color: #b3c0d1;
+  height: 100%;
+  background-color: #b3c0d1;
 }
 .header {
-    background-color: #d3dcd6;
-    padding: 0;
+  background-color: #d3dcd6;
 }
 .main {
-    background-color: #e9eef3;
+  background-color: #e9eef3;
 }
 
 /*头部样式 */
 .middle {
-    text-align: center;
+  text-align: center;
+}
+.middle h2 {
+    margin-top: 14px;
 }
 .loginout {
-    line-height: 60px;
-    text-decoration: none;
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
